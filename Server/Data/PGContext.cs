1a0eebf23e9c47b1a73ea5f5d9f6438553899b9b
Chad <chad27295@gmail.com>
2021-06-20 19:34:57 -0400

    Update PGContext.cs

--- Server/Data/PGContext.cs
+++ Server/Data/PGContext.cs
@@ -18,7 +18,7 @@ namespace BlazorBugOne.Server
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            string myconnnection = (@"Server=localhost;Port=5432;Database=BlazorBugOne;User Id=mradmin;Password=passthemradmin");
+            string myconnnection = (@"Server=localhost;Port=5432;Database=BlazorBugOne;User Id=mradmin;Password=password");
             base.OnConfiguring(optionsBuilder);
 
             //  optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DBTESTCORE2B");
